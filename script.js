1aa8373d2637c980586c668a379c7c01f6a17266
Saulocoimbraa <sauloaugusto1986@gmail.com>
2025-04-02 12:58:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function mostrarQuestoes(categoria) {
             div.innerHTML += `<img src="${questao.imagem}" alt="Imagem da questão" style="max-width: 100%; height: auto; margin-top: 10px;">`;
        }
        //comando
-        div.innerHTML += `<p>${questao.comando}</p>`;
+        document.getElementById("comando").innerText = questao.comando;
     
 
         // Criamos uma div para exibir o resultado da resposta
